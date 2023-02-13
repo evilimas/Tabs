4e18c48c70e9bb0a1ac39f070140f4a65e1d4923
evilimas <eg.vilimas@gmail.com>
2023-02-13 17:12:29 +0100

    init

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict"
+
+// openCity('evt', 'tab1')
+// function openCity(evt, cityName){
+//     var i, tabcontent, tablinks
+//     tabcontent = document.getElementsByClassName("tabcontent")
+//     for(i = 0; i < tabcontent.length; i++){
+//         tabcontent[i].getElementsByClassName.display = "none"
+//     }
+//     tablinks = document.getElementsByClassName('tablinks')
+//     for(i = 0; i < tablinks.length; i++){
+//         tablinks[i].className = tablinks[i].className.replace(" active", "")
+
+//     }
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.cityName += " active";
+
+// }
+openCity('evt', 'tab1');
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
