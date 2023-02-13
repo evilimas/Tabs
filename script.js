51746224735c39e18b9680866acdf3aad1e2299d
evilimas <eg.vilimas@gmail.com>
2023-02-13 21:28:47 +0100

    timeout

--- script.js
+++ script.js
@@ -40,10 +40,43 @@ function openCity(evt, cityName) {
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
+      
     }
     setTimeout(function() {
       document.getElementById(cityName).style.display = "block";
       evt.currentTarget.className += " active";
-    }, 300);
+    }, 500);
+}
+
     
-  }
+//   }
+// openCity('evt', 'tab1');
+// function openCity(evt, cityName) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.className += " active";
+//   }
+// openCity('evt', 'tab1');
+// function openCity(evt, cityName) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     setTimeout(function() {
+//       document.getElementById(cityName).style.display = "block";
+//       evt.currentTarget.className += " active";
+//     }, 1000);
+//   }
